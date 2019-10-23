59f5112f8e08c6a2e8e31e57ee014bbdb9078440
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-22 21:27:33 -0300

    new: structure header

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+        <header>
+            <h2>Teste</h2>
+            <h2>Teste</h2>
+            <h2>Teste</h2>
+        </header>
+    </Container>
+  );
+}
--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import User from '../../components/User';
+import Header from '../../components/Header';
 
 import { Container } from './styles';
 
 export default function Main() {
   return (
     <Container>
+      <Header />
       <User />
     </Container>
   );
