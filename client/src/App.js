fa18dfc333328f964dda097f0057a5d8d10bd401
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-22 21:06:41 -0300

    fix: import routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import Routes from './routes';
 
-import GlobalStyle from '~/style/global';
+import GlobalStyle from './style/global';
 
-import Routes from '~/routes';
 
 function App() {
   return (
