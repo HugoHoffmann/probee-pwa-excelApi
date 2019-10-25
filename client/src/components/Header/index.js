2d0aa0f3b21e6382600d8560445fb84fafd2c332
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-25 20:07:21 -0300

    chore: config Header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-
-import { Container } from './styles';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/img/logo.jpg";
+// import Notifications from '~/components/Notifications';
+import { Container, Content, Profile } from "./styles";
 
 export default function Header() {
   return (
     <Container>
-        <header>
-            <h2>Teste</h2>
-            <h2>Teste</h2>
-            <h2>Teste</h2>
-        </header>
+      <Content>
+        <nav>
+          <Link to="/">
+            <img src={logo} alt="Logo probee" />
+          </Link>
+          <Link to="/users">Usuários</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src={"https://api.adorable.io/avatars/50/abott@adorable.png"}
+              alt=""
+            />
+          </Profile>
+        </aside>
+      </Content>
     </Container>
   );
 }
