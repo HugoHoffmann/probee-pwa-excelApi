8bbbe1c0e4e2f0734a0c2d51ba345d87e888c67e
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-25 21:10:45 -0300

    chore: change header

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
+import Users from './pages/Users';
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Main} />
+                <Route path="/users" component={Users} />
             </Switch>
         </BrowserRouter>
     );
