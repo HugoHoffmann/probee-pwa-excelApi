c98842524450f9e2a0c178ed8d16f70be4f5529a
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-25 21:11:42 -0300

    chore: adjustment Header component

--- client/src/style/global.js
+++ client/src/style/global.js
@@ -19,6 +19,7 @@ export default createGlobalStyle`
     }
     body{
         -webkit-font-smoothing: antialiased;
+        background: #999;
     }
     body, input, button{
         font: 14px 'Roboto', sans-serif;
