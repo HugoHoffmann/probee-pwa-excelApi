29c56ce40667b4b3c73c9d9c28e1b240bea62e3a
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-26 21:47:31 -0300

    chore: add modal component to edit user

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { MdModeEdit, MdDelete } from "react-icons/md";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 import Header from "../../components/Header";
+import Modal from "../../components/Modal";
+import Button from "../../styles/components/button";
 import { api } from "../../services/api";
 
 import { Container, User, ButtonIcon } from "./styles";
 
-export default function Users() {
+export default function Users({closeUserModal}) {
   const [users, setUsers] = useState();
+  const [user, setUser] = useState();
   const [newUser, setNewUser] = useState();
   const [email, setEmail] = useState();
   const idApi = "14B8PtCS-aKwA3TVgfXbp9NdEwqmzft3T-rl-vj00YWY";
@@ -31,21 +34,24 @@ export default function Users() {
   // function handleEdit(){
 
   // }
-  function removeUser(id){
+  function removeUser(id) {
     const usersFilter = users.filter(value => {
       return value.rowIndex !== id;
     });
     setUsers(usersFilter);
   }
-   function handleDelete(rowIndex){
+  function handleDelete(rowIndex) {
     removeUser(rowIndex);
-    toast.success('Usuário excluído com sucesso!')
+    toast.success("Usuário excluído com sucesso!");
     api.delete(`${type}/${rowIndex}?spreadsheetId=${idApi}`);
-
   }
 
-  function handleEdit({rowIndex, nome, email}){
+  function handleEdit({ rowIndex, nome, email }) {
     debugger;
+  }
+
+  function handleInputChange(){
+    // modal edit
   }
 
   return (
@@ -72,16 +78,33 @@ export default function Users() {
                 <strong>{user.nome}</strong>
                 <strong>{user.email}</strong>
                 <strong>
-                  <ButtonIcon >
-                    <MdModeEdit onClick={() => handleEdit(user)}/>
+                  <ButtonIcon>
+                    <MdModeEdit onClick={() => handleEdit(user)} />
                   </ButtonIcon>
-                  <ButtonIcon   >
-                    <MdDelete onClick={() => handleDelete(user.rowIndex)}/>
+                  <ButtonIcon>
+                    <MdDelete onClick={() => handleDelete(user.rowIndex)} />
                   </ButtonIcon>
                 </strong>
               </User>
             ))}
         </ul>
+        <Modal size="big">
+          <h1>Edição Usuário</h1>
+          <form>
+            <span>NOME</span>
+            <input
+              name="user"
+              value={user}
+              onChange={handleInputChange}
+            />
+            <Button onClick={handleEdit} size="big" type="submit">
+              Salvar
+            </Button>
+            <Button onClick={closeUserModal} size="small" color="gray">
+              Cancelar
+            </Button>
+          </form>
+        </Modal>
       </Container>
     </>
   );
