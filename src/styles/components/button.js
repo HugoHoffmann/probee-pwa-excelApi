443f6aac52a87124119bf80878905fa40bfd62f1
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-27 09:34:50 -0300

    chore: create components style button

--- src/styles/components/button.js
+++ src/styles/components/button.js
@@ -45,20 +45,18 @@ const Button = styled.button.attrs({
     transition: background-color 0.15s ease;
     background: #7289da;
     border: 0;
-    color: #fff;
+    color: #333;
     font-size: 12px;
     padding: 0 10px;
     text-transform: uppercase;
     font-weight: 700;
+    &:hover{
+        color: #fff;
+        background: none;
+    }
     ${props => sizes[props.size || 'default']};
     ${props => colors[props.color || 'default']};
-    ${props => props.filled === false && css`
-        background: none;
-        &:hover{
-            background: none;
-            opacity: 0.6;
-        }
-    `};
+    
 `;
 
 export default Button;
